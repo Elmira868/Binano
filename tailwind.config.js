2b864c77cc33a040811da2aa7771bc97a3780c2f
elmira amjadian <elmiraamjadian5@gamil.com>
2025-01-06 19:36:49 +0330

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Vazir-Light": "vazir-light",
+        "Vazir-Medium": "vazir-medium",
+        "Vazir-Bold": "vazir-bold",
+      },
+    },
+  },
+  plugins: [],
+}
+
